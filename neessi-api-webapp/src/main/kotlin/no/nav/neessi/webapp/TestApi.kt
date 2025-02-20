371a2acd6c3173b5913453366e37ec1e81920a22
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-02-20 09:41:58 +0100

    initial 🍇

--- neessi-api-webapp/src/main/kotlin/no/nav/neessi/webapp/TestApi.kt
+++ neessi-api-webapp/src/main/kotlin/no/nav/neessi/webapp/TestApi.kt
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RequestMethod.GET
+import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -65,7 +65,7 @@ class TestApi(
         @Parameter(description = "", required = true) @RequestBody
         payload: TestPayload
     ): ResponseEntity<String> {
-        val body = cpiClient.postTest(payload)
+        val body = cpiClient.getTest(payload)
         return ResponseEntity(body, HttpStatusCode.valueOf(200))
     }
 
