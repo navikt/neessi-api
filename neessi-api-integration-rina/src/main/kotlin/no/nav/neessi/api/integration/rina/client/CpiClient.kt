d74f298b90c50ea3fb6e644ae76caa3196b1f778
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-02-20 09:26:39 +0100

    initial 🍇

--- neessi-api-integration-rina/src/main/kotlin/no/nav/neessi/api/integration/rina/client/CpiClient.kt
+++ neessi-api-integration-rina/src/main/kotlin/no/nav/neessi/api/integration/rina/client/CpiClient.kt
@@ -1,14 +1,33 @@
 package no.nav.neessi.api.integration.rina.client
 
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import no.nav.model.v43.S040
+import no.nav.neessi.api.integration.rina.config.RinaCpiServiceProperties
 import no.nav.neessi.api.integration.rina.v43.documents.s040.cpiRootModel
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.toEntity
 
 @Component
 class CpiClient(
-    val cpiRestClient: RestClient
+    val cpiRestClient: RestClient,
+    val properties: RinaCpiServiceProperties,
 ) {
+
+    val log = logger {}
+
+    fun postTest(payload: TestPayload): String {
+        val uri = payload.path
+        log.info { "Sending test: ${properties.rinaBaseUrl}/$uri" }
+        log.info { }
+        return cpiRestClient
+            .post()
+            .uri("/eessiRest/$payload")
+            .body(payload.payload)
+            .retrieve()
+            .toEntity<String>()
+            .body!!
+    }
 
     fun put(sO40: S040) {
         put(sO40.cpiRootModel)
@@ -17,8 +36,13 @@ class CpiClient(
     private fun put(any: Any) =
         cpiRestClient
             .put()
-            .uri("/cpi")
+            .uri("/eessiRest/Cases/")
             .body(any)
             .retrieve()
 
 }
+
+data class TestPayload(
+    val payload: String,
+    val path: String,
+)
---
+++ neessi-api-webapp/src/main/kotlin/no/nav/neessi/webapp/TestApi.kt
@@ -0,0 +1,46 @@
+package no.nav.neessi.webapp
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import no.nav.neessi.api.integration.rina.client.CpiClient
+import no.nav.neessi.api.integration.rina.client.TestPayload
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod.POST
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestApi(
+    val cpiClient: CpiClient
+) {
+
+    @Unprotected
+    @Operation(
+        summary = "summary",
+        operationId = "operation",
+        description = "desc",
+        responses = [
+            ApiResponse(
+                responseCode = "204",
+                description = "No content"
+            ),
+        ]
+    )
+    @RequestMapping(
+        method = [POST],
+        value = ["/test"],
+        produces = ["application/json"]
+    )
+    fun test(
+        @Parameter(description = "", required = true) @RequestBody
+        payload: TestPayload
+    ): ResponseEntity<String> {
+        val body = cpiClient.postTest(payload)
+        return ResponseEntity(body, HttpStatusCode.valueOf(200))
+    }
+}
+
