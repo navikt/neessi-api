f9a61193585743ddc70ab4e439d3f8841b01e6de
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-02-20 09:52:50 +0100

    initial 🍇

--- neessi-api-webapp/src/main/kotlin/no/nav/neessi/webapp/TestApi.kt
+++ neessi-api-webapp/src/main/kotlin/no/nav/neessi/webapp/TestApi.kt
@@ -10,9 +10,7 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RequestMethod.POST
-import org.springframework.web.bind.annotation.RequestMethod.PUT
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -58,7 +56,7 @@ class TestApi(
         ]
     )
     @RequestMapping(
-        method = [GET],
+        method = [POST],
         value = ["/testGet"],
         produces = ["application/json"]
     )
@@ -83,7 +81,7 @@ class TestApi(
         ]
     )
     @RequestMapping(
-        method = [PUT],
+        method = [POST],
         value = ["/testPut"],
         produces = ["application/json"]
     )
